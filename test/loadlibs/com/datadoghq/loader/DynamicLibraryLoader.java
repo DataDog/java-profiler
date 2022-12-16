d5aca413db39f82de37e8c3bd79065cb0ddbb4db
Jaroslav Bachorik <jaroslav.bachorik@datadoghq.com>
2022-12-16 13:36:08 +0000

    Disable cstack by in libraries test

--- test/loadlibs/com/datadoghq/loader/DynamicLibraryLoader.java
+++ test/loadlibs/com/datadoghq/loader/DynamicLibraryLoader.java
@@ -29,7 +29,7 @@ public class DynamicLibraryLoader {
     private static void startProfilerAndDoWork(String jfrFile) throws Exception {
         JavaProfiler ap = JavaProfiler.getInstance(getJavaProfilerLib());
         Path jfrDump = Paths.get(jfrFile);
-        ap.execute("start,loglevel=TRACE,cpu=1m,wall=~1ms,filter=0,jfr,file=" + jfrDump.toAbsolutePath());
+        ap.execute("start,loglevel=TRACE,cpu=1m,wall=1ms,filter=0,jfr,cstack=no,file=" + jfrDump.toAbsolutePath());
         ap.addThread(ap.getNativeThreadId());
         work();
         ap.stop();
