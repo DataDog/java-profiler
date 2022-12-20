76234d56594124e93a593bfc1d887737025e5c8f
Richard Startin <richard.startin@datadoghq.com>
2022-12-20 16:46:17 +0000

    replace sleep test

--- test/SleepTest.java
+++
@@ -1,6 +0,0 @@
-
-public class SleepTest {
-    public static void main(String[] args) throws InterruptedException {
-        Thread.sleep(Integer.parseInt(args[0]));
-    }
-}
---
+++ test/maven/java/com/datadoghq/profiler/wallclock/CollapsingSleepTest.java
@@ -0,0 +1,27 @@
+package com.datadoghq.profiler.wallclock;
+
+import com.datadoghq.profiler.AbstractProfilerTest;
+import org.junit.jupiter.api.Test;
+import org.openjdk.jmc.common.item.Aggregators;
+import org.openjdk.jmc.common.item.IItemCollection;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class CollapsingSleepTest extends AbstractProfilerTest {
+
+    @Test
+    public void testSleep() throws InterruptedException {
+        registerCurrentThreadForWallClockProfiling();
+        Thread.sleep(1000);
+        stopProfiler();
+        IItemCollection events = verifyEvents("datadog.MethodSample");
+        assertTrue(events.hasItems());
+        assertTrue(events.getAggregate(Aggregators.sum(WEIGHT)).longValue() > 900);
+        assertTrue(events.getAggregate(Aggregators.count()).longValue() > 9);
+    }
+
+    @Override
+    protected String getProfilerCommand() {
+        return "wall=~10ms";
+    }
+}
