90a8d49c2c778d4e68d4b823997a66242587db4d
Jaroslav Bachorik <jaroslav.bachorik@datadoghq.com>
2023-01-09 15:11:37 +0000

    Improve the maven build

---
+++ build_support/make_for_maven.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+HERE=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+(
+    cd $HERE/..
+    OS=$(uname)
+    CPU=$(uname -m)
+    ARCH=
+    case $OS in
+        Linux) ARCH=linux;;
+        Darwin) ARCH=macos;;
+        *) echo "Unsupported OS: $OS"; exit 1;;
+    esac
+
+    case $CPU in
+        x86_64) ARCH+="-x64";;
+        arm64) ARCH+="-arm64";;
+        *) echo "Unsupported CPU: $CPU"; exit 1;;
+    esac
+    make clean all
+
+    TARGET_DIR=src/main/resources/native-libs/$ARCH
+    mkdir -p $TARGET_DIR
+    cp build/libjavaProfiler.so ${TARGET_DIR}/
+)
