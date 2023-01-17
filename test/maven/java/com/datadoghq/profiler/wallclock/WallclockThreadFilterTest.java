b451a66b2d7b3d630e45c2a615fbd8b216723a98
Richard Startin <richard.startin@datadoghq.com>
2023-01-17 15:17:40 +0000

    add test for thread filtering

---
+++ test/maven/java/com/datadoghq/profiler/wallclock/WallclockThreadFilterTest.java
@@ -0,0 +1,40 @@
+package com.datadoghq.profiler.wallclock;
+
+import com.datadoghq.profiler.AbstractProfilerTest;
+import org.junit.jupiter.api.Test;
+import org.openjdk.jmc.common.item.IItem;
+import org.openjdk.jmc.common.item.IItemCollection;
+import org.openjdk.jmc.common.item.IItemIterable;
+import org.openjdk.jmc.common.item.IMemberAccessor;
+import org.openjdk.jmc.common.unit.IQuantity;
+import org.openjdk.jmc.flightrecorder.jdk.JdkAttributes;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class WallclockThreadFilterTest extends AbstractProfilerTest {
+
+    @Test
+    public void test() throws InterruptedException {
+        registerCurrentThreadForWallClockProfiling();
+        Thread.sleep(100);
+        stopProfiler();
+        IItemCollection events = verifyEvents("datadog.MethodSample");
+        for (IItemIterable wallclockSamples : events) {
+            IMemberAccessor<String, IItem> javaThreadNameAccessor = JdkAttributes.EVENT_THREAD_NAME
+                    .getAccessor(wallclockSamples.getType());
+            IMemberAccessor<IQuantity, IItem> javaThreadIdAccessor = JdkAttributes.EVENT_THREAD_ID
+                    .getAccessor(wallclockSamples.getType());
+            for (IItem sample : wallclockSamples) {
+                String javaThreadName = javaThreadNameAccessor.getMember(sample);
+                assertEquals(Thread.currentThread().getName(), javaThreadName);
+                long javaThreadId = javaThreadIdAccessor.getMember(sample).longValue();
+                assertEquals(Thread.currentThread().getId(), javaThreadId);
+            }
+        }
+    }
+
+    @Override
+    protected String getProfilerCommand() {
+        return "wall=~1ms,filter=0";
+    }
+}
