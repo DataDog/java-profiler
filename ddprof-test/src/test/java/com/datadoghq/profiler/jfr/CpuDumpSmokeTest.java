6a6edcc661a7a7f2dd1d6f2acd38823f961721c1
Jaroslav Bachorik <j.bachorik@gmail.com>
2023-12-18 10:17:08 +0100

    Test against JDK 21 too

--- ddprof-test/src/test/java/com/datadoghq/profiler/jfr/CpuDumpSmokeTest.java
+++ ddprof-test/src/test/java/com/datadoghq/profiler/jfr/CpuDumpSmokeTest.java
@@ -2,6 +2,7 @@ package com.datadoghq.profiler.jfr;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Timeout;
+import org.junitpioneer.jupiter.RetryingTest;
 
 public class CpuDumpSmokeTest extends JfrDumpTest {
 
@@ -10,7 +11,7 @@ public class CpuDumpSmokeTest extends JfrDumpTest {
         return "cpu=1ms,cstack=fp";
     }
 
-    @Test
+    @RetryingTest(3)
     @Timeout(value = 60)
     public void test() throws Exception {
         runTest("datadog.ExecutionSample");
