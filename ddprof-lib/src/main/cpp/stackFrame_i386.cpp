397f8c20b0817f56485c84917786f5dbe8abba99
Andrei Pangin <noreply@pangin.pro>
2024-01-16 23:49:01 +0000

    #819: Extend AArch64 stack walking fix to cstack=dwarf

--- ddprof-lib/src/main/cpp/stackFrame_i386.cpp
+++ ddprof-lib/src/main/cpp/stackFrame_i386.cpp
@@ -115,7 +115,7 @@ bool StackFrame::unwindCompiled(NMethod *nm, uintptr_t &pc, uintptr_t &sp,
   return false;
 }
 
-void StackFrame::adjustCompiled(NMethod *nm, const void *pc, uintptr_t &sp) {
+void StackFrame::adjustSP(const void *entry, const void *pc, uintptr_t &sp) {
   // Not needed
 }
 
