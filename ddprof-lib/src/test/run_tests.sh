90563855fb21b4cfdd5a3ca92b9423ec879dfbdf
Jaroslav Bachorik <j.bachorik@gmail.com>
2024-05-27 20:24:04 +0200

    More gtest build compatibilty fixes

--- ddprof-lib/src/test/run_tests.sh
+++ ddprof-lib/src/test/run_tests.sh
@@ -16,11 +16,11 @@ function build_test() {
   # shellcheck disable=SC2038
   files=$(find ${PROJECT_DIR}/build/obj/main/${CONFIG_NAME} -name "*.o" -print | tr '\n' ' ')
   libdir=${GTEST_DIR}/configs/${CONFIG_NAME}/lib
-  g++ -std=c++11 ${ARGS[@]} -o ${TMPDIR}/${NAME}_${CONFIG_NAME} ${PROJECT_DIR}/src/test/cpp/${NAME}.cpp \
+  g++ -std=c++11 -o ${TMPDIR}/${NAME}_${CONFIG_NAME} ${PROJECT_DIR}/src/test/cpp/${NAME}.cpp \
         -I${GTEST_DIR}/googletest/include -I${GTEST_DIR}/googlemock/include \
         -I${PROJECT_DIR}/src/main/cpp ${libdir}/libgtest.a \
         ${libdir}/libgtest_main.a ${libdir}/libgmock.a \
-        ${libdir}/libgmock_main.a $files -lpthread -lm -lrt -Wl,--no-as-needed -ldl -pthread
+        ${libdir}/libgmock_main.a $files -lpthread -lm -ldl ${ARGS[@]}  -pthread
 }
 
 function build_and_run() {
