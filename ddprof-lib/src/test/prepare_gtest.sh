f9b3e7b2909ad5db6ab8f2f5a1e39b702edf12cd
r1viollet <erwan.viollet@datadoghq.com>
2024-05-28 10:06:41 +0200

    Minor fix on folder used to run the gtest build

--- ddprof-lib/src/test/prepare_gtest.sh
+++ ddprof-lib/src/test/prepare_gtest.sh
@@ -1,12 +1,27 @@
 #!/usr/bin/env bash
-# set -x
+set -eo pipefail
+
 CONFIG_NAME=$1
 GTEST_DIR=$2
 
-CFG_DIR="${GTEST_DIR}"/configs/"${CONFIG_NAME}"
-if [ ! -d "${CFG_DIR}/lib" ]; then
-    mkdir -p $CFG_DIR
-    cd "${GTEST_DIR}"/configs/"${CONFIG_NAME}"
-    cmake ../.. 
-    make
+CFG_DIR="${GTEST_DIR}/configs/${CONFIG_NAME}"
+
+echo "Starting script with CONFIG_NAME=${CONFIG_NAME} and GTEST_DIR=${GTEST_DIR}"
+
+if [ ! -d "${CFG_DIR}" ]; then
+    mkdir -p "${CFG_DIR}"
 fi
+cd "${CFG_DIR}"
+echo "Running cmake in ${CFG_DIR}"
+cmake ${GTEST_DIR}/../../../src/test -DCMAKE_BUILD_TYPE=${CONFIG_NAME}
+if [ $? -ne 0 ]; then
+    echo "cmake failed"
+    exit 1
+fi
+echo "Running make in ${CFG_DIR}"
+make
+if [ $? -ne 0 ]; then
+    echo "make failed"
+    exit 1
+fi
+echo "Build completed successfully."
