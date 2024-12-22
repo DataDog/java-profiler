34ff5f09ad34dfbb2b7832a3e144448e10a7b57d
Long Yang <yibo.yl@alibaba-inc.com>
2024-12-23 05:32:34 +0800

    #1089: Fix stack trace memory pollution

--- ddprof-lib/src/main/cpp/arch.h
+++ ddprof-lib/src/main/cpp/arch.h
@@ -1,17 +1,6 @@
 /*
- * Copyright 2017 Andrei Pangin
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright The async-profiler authors
+ * SPDX-License-Identifier: Apache-2.0
  */
 
 #ifndef _ARCH_H
@@ -19,11 +8,18 @@
 
 #include <stddef.h>
 
+#ifndef unlikely
+#  define unlikely(x)  (__builtin_expect(!!(x), 0))
+#endif
+
+#define callerPC()     __builtin_return_address(0)
+
 #ifdef _LP64
 #  define LP64_ONLY(code) code
 #else // !_LP64
 #  define LP64_ONLY(code)
 #endif // _LP64
+
 
 typedef unsigned char u8;
 typedef unsigned short u16;
