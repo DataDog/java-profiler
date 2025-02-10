5e17d7fe2b601eb0c7532e5f46aec64fb63d4772
Jaroslav Bachorik <j.bachorik@gmail.com>
2025-02-10 17:43:10 +0100

    Restructure CI workflow and improve JDK caching

---
+++ .github/scripts/prepare_reports.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -e
+mkdir -p reports
+cp /tmp/hs_err* reports/ || true
+cp ddprof-test/build/hs_err* reports/ || true
+cp -r ddprof-lib/build/tmp reports/native_build || true
+cp -r ddprof-test/build/reports/tests reports/tests || true
+cp -r /tmp/recordings reports/recordings || true
+find ddprof-lib/build -name 'libjavaProfiler.*' -exec cp {} reports/ \; || true
