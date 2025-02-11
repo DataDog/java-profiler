679f73149b9e60b9dca3e48026f52aba8ac17721
Jaroslav Bachorik <jaroslav.bachorik@datadoghq.com>
2025-02-11 16:48:02 +0100

    Restructure CI workflow and improve JDK caching (#178)

--- .github/scripts/java_setup.sh
+++ .github/scripts/java_setup.sh
@@ -1,14 +1,76 @@
 #!/usr/bin/env bash
 
 function prepareJdk() {
-  local jdk_distro=$1
-  local target_path=$2
+  local variant=$1
+  local arch=$2
+  local version=${variant%%-*}
+  local qualifier=${variant#*-}
 
-  if [ -e "$target_path/bin/java" ]; then
-    echo "JDK $jdk_distro already installed at $jdk_path"
+  local target_path="${GITHUB_WORKSPACE}/${JDKS_DIR}/jdk${variant}"
+
+  mkdir -p ${target_path}
+
+  if [[ ${qualifier} == "librca" ]] && [[ "${arch}" =~ "-musl" ]]; then
+    URL_VAR="JAVA_${version}_MUSL_URL"
+    URL="${!URL_VAR}"
+    if [[ -z "${URL}" ]]; then
+      echo "Musl/Liberica JDK URL not found for ${arch}/${variant}"
+      exit 1
+    fi
+    curl -L --fail "${URL}" | tar -xvzf - -C ${target_path} --strip-components 1
     return
   fi
 
-  echo 'n' | sdk install java ${jdk_distro}
-  ln -s ~/.sdkman/candidates/java/${jdk_distro} ${target_path}
+  if [[ ${qualifier} == "orcl" ]]; then
+    if [[ ${version} == "8" ]]; then
+      mkdir -p "${target_path}"
+      curl -L --fail "${JAVA_8_ORACLE_URL}" | sudo tar -xvzf - -C ${target_path} --strip-components 1
+      return
+    else
+      echo "Oracle JDK 8 only!"
+      exit 1
+    fi
+  fi
+
+  if [[ ${qualifier} == "zing" ]]; then
+    URL_VAR="JAVA_${version}_ZING_URL"
+    if [[ "${arch}" == "aarch64" ]]; then
+      URL_VAR="JAVA_${version}_ZING_AARCH64_URL"
+    fi
+
+    URL="${!URL_VAR}"
+    if [[ -z "${URL}" ]]; then
+      echo "Zing JDK URL not found for ${variant}"
+      exit 1
+    fi
+    curl -L --fail "${URL}" | sudo tar -xvzf - -C ${target_path} --strip-components 1
+    if [[ "${arch}" != "aarch64" ]]; then
+      # rename the bundled libstdc++.so to avoid conflicts with the system one
+      sudo mv ${target_path}/etc/libc++/libstdc++.so.6 ${target_path}/etc/libc++/libstdc++.so.6.bak
+    fi
+    return
+  fi
+
+  # below the installation of the SDKMAN-managed JDK
+  source ~/.sdkman/bin/sdkman-init.sh
+
+  local suffix="tem"
+  local versionVar="JAVA_${version}_VERSION"
+  if [[ "${qualifier}" == "j9" ]]; then
+    suffix="sem"
+    versionVar="JAVA_${version}_J9_VERSION"
+  elif [[ "${qualifier}" == "graal" ]]; then
+    suffix="graal"
+    versionVar="JAVA_${version}_GRAAL_VERSION"
+  fi
+
+  local distro_base
+  distro_base="${!versionVar}"
+  local jdk_distro="${distro_base}-${suffix}"
+
+  echo 'n' | sdk install java ${jdk_distro} > /dev/null
+
+  rm -rf ${target_path}
+  mkdir -p "$(dirname ${target_path})"
+  mv ${SDKMAN_DIR}/candidates/java/${jdk_distro} ${target_path}
 }
