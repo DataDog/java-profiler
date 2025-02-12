async-profiler/async-profiler

Parse both .rela.dyn and .rela.plt of libraries. (#1074)

src/symbols_linux.cpp
@@ -145,22 +145,29 @@ typedef Elf32_Dyn  ElfDyn;
 
 #if defined(__x86_64__)
 #  define R_GLOB_DAT R_X86_64_GLOB_DAT
+#  define R_ABS64 R_X86_64_64
 #elif defined(__i386__)
 #  define R_GLOB_DAT R_386_GLOB_DAT
+#  define R_ABS64 -1
 #elif defined(__arm__) || defined(__thumb__)
 #  define R_GLOB_DAT R_ARM_GLOB_DAT
+#  define R_ABS64 -1
 #elif defined(__aarch64__)
 #  define R_GLOB_DAT R_AARCH64_GLOB_DAT
+#  define R_ABS64 R_AARCH64_ABS64
 #elif defined(__PPC64__)
 #  define R_GLOB_DAT R_PPC64_GLOB_DAT
+#  define R_ABS64 -1
 #elif defined(__riscv) && (__riscv_xlen == 64)
 // RISC-V does not have GLOB_DAT relocation, use something neutral,
 // like the impossible relocation number.
 #  define R_GLOB_DAT -1
+#  define R_ABS64 -1
 #elif defined(__loongarch_lp64)
 // LOONGARCH does not have GLOB_DAT relocation, use something neutral,
 // like the impossible relocation number.
 #  define R_GLOB_DAT -1
+#  define R_ABS64 -1
 #else
 #  error "Compiling on unsupported arch"
 #endif
@@ -385,6 +392,21 @@ void ElfParser::parseDynamicSection() {
         }
 
         const char* base = this->base();
+        if (rel != NULL && relsz != 0) {
+            // If a shared library is built without PLT (-fno-plt), relocation entries for imports
+            // can be found in .rela.dyn. However, if both sections exist, .rela.plt entries
+            // should take precedence, that's why we parse .rela.dyn first.
+            for (size_t offs = relcount * relent; offs < relsz; offs += relent) {
+                ElfRelocation* r = (ElfRelocation*)(rel + offs);
+                if (ELF_R_TYPE(r->r_info) == R_GLOB_DAT || ELF_R_TYPE(r->r_info) == R_ABS64) {
+                    ElfSymbol* sym = (ElfSymbol*)(symtab + ELF_R_SYM(r->r_info) * syment);
+                    if (sym->st_name != 0) {
+                        _cc->addImport((void**)(base + r->r_offset), strtab + sym->st_name);
+                    }
+                }
+            }
+        }
+
         if (jmprel != NULL && pltrelsz != 0) {
             // Parse .rela.plt table
             for (size_t offs = 0; offs < pltrelsz; offs += relent) {
@@ -393,18 +415,6 @@ void ElfParser::parseDynamicSection() {
                 if (sym->st_name != 0) {
                     _cc->addImport((void**)(base + r->r_offset), strtab + sym->st_name);
                 }
-            }
-        } else if (rel != NULL && relsz != 0) {
-            // Shared library was built without PLT (-fno-plt)
-            // Relocation entries have been moved from .rela.plt to .rela.dyn
-            for (size_t offs = relcount * relent; offs < relsz; offs += relent) {
-                ElfRelocation* r = (ElfRelocation*)(rel + offs);
-                if (ELF_R_TYPE(r->r_info) == R_GLOB_DAT) {
-                    ElfSymbol* sym = (ElfSymbol*)(symtab + ELF_R_SYM(r->r_info) * syment);
-                    if (sym->st_name != 0) {
-                        _cc->addImport((void**)(base + r->r_offset), strtab + sym->st_name);
-                    }
-               }
             }
         }
     }

DataDog/java-profiler

ddprof-lib/src/main/cpp/symbols_linux.cpp
@@ -1,17 +1,6 @@
 /*
- * Copyright 2017 Andrei Pangin
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright The async-profiler authors
+ * SPDX-License-Identifier: Apache-2.0
  */
 
 #ifdef __linux__
@@ -192,21 +181,25 @@ void ElfParser::parseDynamicSection() {
       loadSymbolTable(symtab, syment * nsyms, syment, strtab);
     }
 
+    if (rel != NULL && relsz != 0) {
+      // If a shared library is built without PLT (-fno-plt), relocation entries for imports
+      // can be found in .rela.dyn. However, if both sections exist, .rela.plt entries
+      // should take precedence, that's why we parse .rela.dyn first.
+      for (size_t offs = relcount * relent; offs < relsz; offs += relent) {
+        ElfRelocation *r = (ElfRelocation *)(rel + offs);
+        if (ELF_R_TYPE(r->r_info) == R_GLOB_DAT || ELF_R_TYPE(r->r_info) == R_ABS64) {
+          ElfSymbol *sym = (ElfSymbol *)(symtab + ELF_R_SYM(r->r_info) * syment);
+          if (sym->st_name != 0) {
+            _cc->addImport((void **)(_base + r->r_offset), strtab + sym->st_name);
+          }
+        }
+      }
+    }
+
     if (jmprel != NULL && pltrelsz != 0) {
       // Parse .rela.plt table
       for (size_t offs = 0; offs < pltrelsz; offs += relent) {
         ElfRelocation *r = (ElfRelocation *)(jmprel + offs);
-        ElfSymbol *sym = (ElfSymbol *)(symtab + ELF_R_SYM(r->r_info) * syment);
-        if (sym->st_name != 0) {
-          _cc->addImport((void **)(_base + r->r_offset), strtab + sym->st_name);
-        }
-      }
-    } else if (rel != NULL && relsz != 0) {
-      // Shared library was built without PLT (-fno-plt)
-      // Relocation entries have been moved from .rela.plt to .rela.dyn
-      for (size_t offs = relcount * relent; offs < relsz; offs += relent) {
-        ElfRelocation *r = (ElfRelocation *)(rel + offs);
-        if (ELF_R_TYPE(r->r_info) == R_GLOB_DAT) {
           ElfSymbol *sym =
               (ElfSymbol *)(symtab + ELF_R_SYM(r->r_info) * syment);
           if (sym->st_name != 0) {
@@ -216,7 +209,6 @@ void ElfParser::parseDynamicSection() {
         }
       }
     }
-  }
 }
 
 void ElfParser::parseDwarfInfo() {