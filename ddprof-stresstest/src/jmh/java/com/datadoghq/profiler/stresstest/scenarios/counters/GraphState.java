a358978031074e6788fd2dc41927c27497e10752
Jaroslav Bachorik <jaroslav.bachorik@datadoghq.com>
2025-07-04 14:30:15 +0200

    Add the micro-benchmark for thread filtering (#237)
    * Add the micro-benchmark for thread filtering
    
    * Do not test for obviously invalid thread id
    
    * Relax threadfilter mem order

--- ddprof-stresstest/src/jmh/java/com/datadoghq/profiler/stresstest/scenarios/counters/GraphState.java
+++ ddprof-stresstest/src/jmh/java/com/datadoghq/profiler/stresstest/scenarios/counters/GraphState.java
@@ -1,4 +1,4 @@
-package com.datadoghq.profiler.stresstest.scenarios;
+package com.datadoghq.profiler.stresstest.scenarios.counters;
 
 import com.datadoghq.profiler.stresstest.Configuration;
 import org.openjdk.jmh.annotations.*;
