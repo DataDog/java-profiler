5b0334ab66a320846ffd18194cbab74c3b0e44e3
Zhengyu Gu <zhengyu.gu@datadoghq.com>
2026-01-16 10:36:45 -0500

    Use default cstack for tests and improve stackwalk test coverage (#331)

--- ddprof-test/src/test/java/com/datadoghq/profiler/jfr/ObjectSampleDumpSmokeTest.java
+++ ddprof-test/src/test/java/com/datadoghq/profiler/jfr/ObjectSampleDumpSmokeTest.java
@@ -4,10 +4,17 @@ import com.datadoghq.profiler.Platform;
 
 import org.junit.jupiter.api.Assumptions;
 import org.junit.jupiter.api.Timeout;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.api.TestTemplate;
 
-import org.junitpioneer.jupiter.RetryingTest;
+import com.datadoghq.profiler.junit.CStack;
+import com.datadoghq.profiler.junit.RetryTest;
 
 public class ObjectSampleDumpSmokeTest extends JfrDumpTest {
+    public ObjectSampleDumpSmokeTest(@CStack String cstack) {
+        super(cstack);
+    }
+
     @Override
     protected boolean isPlatformSupported() {
         return !Platform.isJavaVersion(8) && !Platform.isJ9();
@@ -18,9 +25,11 @@ public class ObjectSampleDumpSmokeTest extends JfrDumpTest {
         return "memory=32:a";
     }
 
-    @RetryingTest(5)
+    @RetryTest(5)
     @Timeout(value = 300)
-    public void test() throws Exception {
+    @TestTemplate
+    @ValueSource(strings = {"vm", "vmx", "fp", "dwarf"})
+    public void test(@CStack String cstack) throws Exception {
         runTest("datadog.ObjectSample", 3, "method3");
     }
 }
--- ddprof-test/src/test/java/com/datadoghq/profiler/jfr/WallclockDumpSmokeTest.java
+++ ddprof-test/src/test/java/com/datadoghq/profiler/jfr/WallclockDumpSmokeTest.java
@@ -1,19 +1,27 @@
 package com.datadoghq.profiler.jfr;
 
 import org.junit.jupiter.api.Timeout;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.api.TestTemplate;
 
-import org.junitpioneer.jupiter.RetryingTest;
+import com.datadoghq.profiler.junit.CStack;
+import com.datadoghq.profiler.junit.RetryTest;
 
 public class WallclockDumpSmokeTest extends JfrDumpTest {
+    public WallclockDumpSmokeTest(@CStack String cstack) {
+        super(cstack);
+    }
 
     @Override
     protected String getProfilerCommand() {
         return "wall=5ms";
     }
 
-    @RetryingTest(3)
+    @RetryTest(3)
     @Timeout(value = 60)
-    public void test() throws Exception {
+    @TestTemplate
+    @ValueSource(strings = {"vm", "vmx", "fp", "dwarf"})
+    public void test(@CStack String cstack) throws Exception {
         registerCurrentThreadForWallClockProfiling();
         runTest("datadog.MethodSample");
     }
