a3426c43e73bcb9c44b0915b613d1350e9ca10d5
Zhengyu Gu <zhengyu.gu@datadoghq.com>
2026-01-28 15:51:30 +0000

    Add new file

---
+++ ddprof-lib/src/main/cpp/threadState.inline.h
@@ -0,0 +1,31 @@
+#ifndef JAVA_PROFILER_LIBRARY_THREAD_STATE_INLINE_H
+#define JAVA_PROFILER_LIBRARY_THREAD_STATE_INLINE_H
+
+
+#include "threadState.h"
+#include "vmStructs.h"
+#include "thread.h"
+
+inline ExecutionMode getThreadExecutionMode() {
+  VMThread* vm_thread = VMThread::current();
+  if (vm_thread == nullptr) {
+    return ExecutionMode::UNKNOWN;
+  }
+  ProfiledThread *prof_thread = ProfiledThread::currentSignalSafe();
+  bool is_java_thread = prof_thread != nullptr && prof_thread->isJavaThread();
+  if (is_java_thread) {
+    int raw_thread_state = vm_thread->state();
+
+    // Java threads: [4, 12) = [_thread_in_native, _thread_max_state)
+    // JVM internal threads: 0 or outside this range
+    is_java_thread = raw_thread_state >= 4 && raw_thread_state < 12;
+
+    return is_java_thread ? convertJvmExecutionState(raw_thread_state)
+                        : ExecutionMode::JVM;
+  } else {
+    // It is a JVM internal thread
+    return ExecutionMode::JVM;
+  }
+}
+
+#endif // JAVA_PROFILER_LIBRARY_THREAD_STATE_INLINE_H
