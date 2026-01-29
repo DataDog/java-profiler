d303ae2c2deec076ff907ce0071adce33428aac3
Zhengyu Gu <zhengyu.gu@datadoghq.com>
2026-01-29 00:24:03 +0000

    Fix

--- ddprof-lib/src/main/cpp/stackWalker.cpp
+++ ddprof-lib/src/main/cpp/stackWalker.cpp
@@ -89,7 +89,7 @@ int StackWalker::walkFP(void* ucontext, const void** callchain, int max_depth, S
     // Walk until the bottom of the stack or until the first Java frame
     while (depth < actual_max_depth) {
         if (CodeHeap::contains(pc) && !(depth == 0 && frame.unwindAtomicStub(pc)) &&
-            VMThread::current() != nullptr) {  // If it is not a Java thread, it cannot have Java frame
+            VMThread::current() != nullptr) {  // If it is not a JVM thread, it cannot have Java frame
             java_ctx->set(pc, sp, fp);
             break;
         }
@@ -147,7 +147,7 @@ int StackWalker::walkDwarf(void* ucontext, const void** callchain, int max_depth
     // Walk until the bottom of the stack or until the first Java frame
     while (depth < actual_max_depth) {
         if (CodeHeap::contains(pc) && !(depth == 0 && frame.unwindAtomicStub(pc)) &&
-            VMThread::current() != nullptr) {  // If it is not a Java thread, it cannot have Java frame
+            VMThread::current() != nullptr) {  // If it is not a JVM thread, it cannot have Java frame
             // Don't dereference pc as it may point to unreadable memory
             // frame.adjustSP(page_start, pc, sp);
             java_ctx->set(pc, sp, fp);
