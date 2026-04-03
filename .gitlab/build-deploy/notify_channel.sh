3806a73b0c4ca55d9bf8250f66ee592257734c23
Jaroslav Bachorik <jaroslav.bachorik@datadoghq.com>
2026-04-03 18:46:40 +0200

    ci: add branch link to slack build notifications

--- .gitlab/build-deploy/notify_channel.sh
+++ .gitlab/build-deploy/notify_channel.sh
@@ -16,20 +16,23 @@ PIPELINE_LINK="<$PIPELINE_URL|pipeline #$CI_PIPELINE_ID>"
 COMMIT_URL="https://github.com/DataDog/java-profiler/commit/$COMMIT_SHA"
 COMMIT_LINK="<$COMMIT_URL|${COMMIT_SHA:0:8}>"
 
+BRANCH_URL="https://github.com/DataDog/java-profiler/tree/$CI_COMMIT_BRANCH"
+BRANCH_LINK="<$BRANCH_URL|$CI_COMMIT_BRANCH>"
+
 # get status from argument
 STATUS=$1
 VERSION=$2
 
 if [[ $STATUS == "success" ]]; then
   if [[ ! $VERSION =~ .*?-SNAPSHOT ]]; then
-    MESSAGE_TEXT=":tada: Release succeeded for ${VERSION} (commit=$COMMIT_LINK, pipeline=$PIPELINE_LINK)
+    MESSAGE_TEXT=":tada: Release succeeded for ${VERSION} (branch=$BRANCH_LINK, commit=$COMMIT_LINK, pipeline=$PIPELINE_LINK)
 
 :point_up: Now you can publish the artifacts from <https://oss.sonatype.org/#stagingRepositories|OSS Sonatype>"
   else
-    MESSAGE_TEXT=":done: Build succeeded for ${VERSION} (commit=$COMMIT_LINK, pipeline=$PIPELINE_LINK)"
+    MESSAGE_TEXT=":done: Build succeeded for ${VERSION} (branch=$BRANCH_LINK, commit=$COMMIT_LINK, pipeline=$PIPELINE_LINK)"
   fi
 else
-  MESSAGE_TEXT=":better-siren: Build failed for ${VERSION} (commit=$COMMIT_LINK, pipeline=$PIPELINE_LINK)"
+  MESSAGE_TEXT=":better-siren: Build failed for ${VERSION} (branch=$BRANCH_LINK, commit=$COMMIT_LINK, pipeline=$PIPELINE_LINK)"
 fi
 
 postmessage "$SLACK_CHANNEL" "$MESSAGE_TEXT" "$STATUS"
