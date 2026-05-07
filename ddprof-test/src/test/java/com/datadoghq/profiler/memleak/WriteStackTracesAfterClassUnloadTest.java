283d9c4e783a00b729072269f31816f97a29cc61
Jaroslav Bachorik <jaroslav.bachorik@datadoghq.com>
2026-05-07 18:57:59 +0200

    ci-watch: fix compileTestJava — profiler.resetThreadContext() (cycle 1)

--- ddprof-test/src/test/java/com/datadoghq/profiler/memleak/WriteStackTracesAfterClassUnloadTest.java
+++ ddprof-test/src/test/java/com/datadoghq/profiler/memleak/WriteStackTracesAfterClassUnloadTest.java
@@ -131,7 +131,7 @@ public class WriteStackTracesAfterClassUnloadTest extends AbstractProfilerTest {
         try {
           profiler.stop();
         } finally {
-          resetThreadContext();
+          profiler.resetThreadContext();
         }
       }
 
