08bbdf171a17e6d10852b129645ae75cd6c0cc22
Zhengyu Gu <zhengyu.gu@datadoghq.com>
2026-05-29 21:14:14 +0000

    Fix duplicated cookies

---
+++ ddprof-lib/src/main/cpp/signalCookie.cpp
@@ -0,0 +1,17 @@
+#include "signalCookie.h"
+
+namespace SignalCookie {
+    namespace detail {
+        // Place tags in a named section on Linux to prevent LTO from merging
+        // or reordering them across TUs (their addresses must be unique per DSO).
+#ifdef __linux__
+        [[gnu::section(".data.signal_cookie")]] char cpu_tag;
+        [[gnu::section(".data.signal_cookie")]] char wallclock_tag;
+#else
+        char cpu_tag;
+        char wallclock_tag;
+#endif
+    }
+    void* cpu()       { return &detail::cpu_tag; }
+    void* wallclock() { return &detail::wallclock_tag; }
+}
